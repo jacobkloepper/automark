2fae2587adb33e765f473fe2c12ca8675996ccfd
Jacob Kloepper <kloepperwjacob@gmail.com>
2020-12-30 03:03:51 -0800

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# Automark
+
+# Goal: input your class names and their marking schemes
+# Each time you get a mark back, input it under its section in the scheme
+# At any point in the semester, check your grade
+
+if __name__ == '__main__':
+    print("initial commit")
---
+++ readme.txt
@@ -0,0 +1,15 @@
+Automark
+
+Options:
+
+Add new class
+	- Add class name
+	- Input syllabus marking scheme
+
+Check/add to class
+	- Input class name
+	- Input syllabus section
+	- Input mark
+
+Look at grades
+	- Output each syllabus section and the overall grade
